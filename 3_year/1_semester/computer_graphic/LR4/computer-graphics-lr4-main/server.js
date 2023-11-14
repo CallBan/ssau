818715b72c90ec6ce43d1ac7ab61fa278f40f4f7
DmitryKolbanov <kolbanov.dmitry@yandex.ru>
2023-11-14 11:46:10 +0400

    delete not completed

--- 3_year/1_semester/computer_graphic/LR4/computer-graphics-lr4-main/server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-   
-app.use(express.static(__dirname + '/public'));
-app.use("/", (req, res) => {
-    res.redirect("index.html")
-});
-
-app.listen(3000);
