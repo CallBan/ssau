c65161cc420c6ff3650a456f5374abe9c9f253fd
DmitryKolbanov <kolbanov.dmitry@yandex.ru>
2023-11-26 17:35:55 +0400

    _

---
+++ 3 курс/1 семестр/Базы данных/ЛР5/requests.sql
@@ -0,0 +1,149 @@
+-- ПРЕДСТАВЛЕНИЯ
+-- 1. Создать два не обновляемых представления, возвращающих пользователю
+-- результат из нескольких таблиц, с разными алгоритмами обработки представления.
+
+-- Первое необновляемое представление
+create algorithm=merge view ViewStudentContract
+as select student.full_name as "Студент", contract.type as "Форма обучения"
+from student
+join contract on student.student_id =  contract.student_id;
+
+select * from ViewStudentContract;
+
+-- Второе необновляемое представление
+create algorithm=temptable view ViewYearScholarship
+as select student.year as "Курс", sum(category.scholarship) as "Сумма стипендии"
+from student
+join session_result on student.student_id = session_result.student_id
+join category on session_result.category_id = category.category_id
+group by student.year order by student.year;
+
+select * from ViewYearScholarship;
+
+-- 2. Создать обновляемое представление, не позволяющее выполнить команду INSERT.
+create view ViewScholarship(category_name, category_scholarship)
+as select category.name, category.scholarship
+from category;
+
+select * from ViewScholarship;
+
+insert into ViewScholarship(category_name, category_scholarship) value
+("Социальная", 3400);
+
+drop view ViewScholarship;
+-- 3. Создать обновляемое представление, позволяющее выполнить команду INSERT.
+create view ViewScholarship(category_id, category_name, category_scholarship)
+as select category.category_id, category.name, category.scholarship
+from category;
+
+select * from ViewScholarship;
+
+insert into ViewScholarship(category_id, category_name, category_scholarship) value
+(6, "Социальная", 3400);
+
+-- 4. Создать вложенное обновляемое представление с проверкой ограничений (WITH CHECK OPTION). 
+create view ViewStudent(student_id, full_name, year, group_id)
+as select student.student_id, student.full_name, student.year, student.group_id
+from student
+where year = 1
+with check option;
+
+select * from ViewStudent;
+
+insert into ViewStudent(student_id, full_name, year, group_id) value
+	(32, "Шевелев Павел Тимурович", 2, 2);
+
+select * from student;
+    
+-- ТРАНЗАКЦИИ
+-- 5. Выберите любую таблицу, созданную в предыдущих лабораторных работах.
+-- Создайте транзакцию, произведите ее откат и фиксацию:
+select * from category;
+set SQL_SAFE_UPDATES = 0;
+-- a. Отключите режим автоматического завершения;
+set AUTOCOMMIT = 0;
+start transaction;
+-- b. Добавьте в выбранную таблицу новые записи, проверьте добавились ли они;
+update category
+set scholarship =  4000
+where scholarship =  3600;
+select * from category;
+-- c. Произведите откат транзакции, т. е. отмену произведенных действий;
+rollback;
+-- d. Откатите транзакцию оператором ROLLBACK(изменения не сохранились);
+select * from category;
+-- e. Воспроизведите транзакцию и сохраните действия оператором COMMIT.
+start transaction;
+update category
+set scholarship =  4000
+where scholarship =  3600;
+commit;
+select * from category;
+
+-- 6. Продемонстрировать возможность чтения незафиксированных изменений при
+-- использовании уровня изоляции READ UNCOMMITTED и отсутствие такой
+-- возможности при уровне изоляции READ COMMITTED.
+
+-- Пример с READ UNCOMMITTED
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+START TRANSACTION;
+
+select * from category;
+
+update category
+set scholarship =  3000
+where scholarship =  4000;
+
+select * from category;
+
+ROLLBACK;
+
+-- Пример с READ COMMITTED
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+START TRANSACTION;
+
+select * from category;
+
+update category
+set scholarship =  3000
+where scholarship =  4000;
+
+select * from category;
+
+ROLLBACK;
+
+-- 7. Продемонстрировать возможность записи в уже прочитанные данные при
+-- использовании уровня изоляции READ COMMITTED и отсутствие такой
+-- возможности при уровне изоляции REPEATABLE READ. 
+
+-- Пример с READ COMMITTED
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+START TRANSACTION;
+
+select * from category;
+
+insert into ViewScholarship(category_id, category_name, category_scholarship) value
+(7, "Суперсоциальная", 34000);
+
+select * from category;
+
+commit;
+
+delete from category where category_id = 7;
+
+-- Пример с REPEATABLE READ. 
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
+START TRANSACTION;
+
+select * from category;
+
+insert into ViewScholarship(category_id, category_name, category_scholarship) value
+(7, "Суперсоциальная", 34000);
+
+select * from category;
+
+commit;
---
+++ 3 курс/1 семестр/Компьютерная графика/ЛР5/script.js
@@ -0,0 +1,124 @@
+let root = document.getElementById('root')
+let canvas = document.getElementById('canvasik')
+let ctx = canvas.getContext('2d');
+
+function slider(id, value, min, max, step) {
+    let slider = document.getElementById(id)
+    slider.type = "range"
+    slider.max = max ?? 2 * 3.14 * 100
+    slider.min = min ?? 0
+    slider.step = step ?? 1
+    slider.value = value ?? 0;
+    return slider
+}
+
+class Point {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+function transform(point, a, b, g, scale, shifts) {
+    let X = point.x * Math.cos(a) + point.y * Math.sin(a)
+    let Y = -point.x * Math.sin(a)*Math.cos(b) + point.y * Math.cos(a)*Math.cos(b) + point.z*Math.sin(b)
+    let Z = point.x*Math.sin(a)*Math.sin(b) - point.y*Math.cos(a)*Math.sin(b) + point.z*Math.cos(b)
+
+    X = X * Math.cos(g) - Z * Math.sin(g)
+    Z = X * Math.sin(g) + Z * Math.cos(g)
+
+    X = X + shifts[0]
+    Y = Y + shifts[1]
+    Z = Z + shifts[2]
+
+    return new Point(X * scale, Y * scale, Z * scale)
+}
+
+function generatePoints(R, A = 6, K = 3) {
+    let alphaSteps = 30;
+    let betaSteps = 30;
+    
+    let layers = []
+    for(let j = 0; j < alphaSteps; j++) {
+        let layer = []
+        for(let i = 0; i < betaSteps; i++) {
+            let beta = i * 2 * Math.PI / betaSteps
+            let alpha = j * Math.PI / alphaSteps
+        
+            let { cos, sin, abs, sqrt, pow } = Math;
+
+            x = R * sin(alpha)*cos(beta)*(1 + 0.5 * abs(sin(K * beta)));
+            y = R * sin(alpha)*sin(beta)*(1 + 0.5 * abs(sin(K * beta)));
+            z = -R * sqrt(A * pow(sin(0.5 * alpha), 1.5)) + 1.5 * R
+
+            layer.push(new Point(x, y, z))
+        }
+        layers.push(layer)
+    }
+    let polygons_ = polygons(layers)
+    return polygons_;
+}
+
+function polygons(layers) {
+    let polygons = []
+    for(let i = 0; i < layers.length - 1; i++) {
+        let layer = layers[i]
+        let next_layer = layers[i + 1]
+        for(let j = 0; j < layer.length; j++) {
+            let n = layer.length;
+            let polygonPoints = [
+                layer[j],
+                layer[(j+1) % n],
+                next_layer[j],
+                next_layer[(j+1) % n]
+            ];
+            polygons.push(polygonPoints)
+        }
+    }
+    return polygons
+}
+
+function drawPoints(points) {
+    ctx.beginPath();
+    let dx = canvas.width / 2
+    let dy = canvas.height / 2
+    for(let i = 0; i < points.length; i++) {
+        ctx.lineTo(dx + points[i].x, dy + points[i].y)
+    }
+    ctx.stroke()
+}
+
+function drawPolygon(polygon) {
+    let points = polygon;
+    let angles = slidersAngle.map(slider => slider.value / 100);
+    let shifts = slidersShift.map(shifts => +shifts.value)
+    let t_points = points.map(p => transform(p, ...angles, scaleSlider.value, shifts))
+    drawPoints(t_points);
+}
+
+function drawPolygons() {
+    ctx.clearRect(0, 0, 900, 900)
+    let polys = generatePoints(rSlider.value, aSlider.value, kSlider.value)
+    polys.forEach(poly => drawPolygon(poly))
+}
+
+let slidersAngle = [slider("alpha"), slider("beta"), slider("gamma")]
+slidersAngle.forEach(slider => slider.oninput = drawPolygons)
+
+let slidersShift = [slider('dx', 0, -300, 300), slider('dy', 0, -300, 300), slider('dz', 0, -300, 300)]
+slidersShift.forEach(slider =>slider.oninput = drawPolygons)
+
+let scaleSlider = slider('scale', 1, 0.5, 5, 0.1)
+scaleSlider.oninput = drawPolygons
+
+let rSlider = slider('R', 100, 30, 600, 1);
+rSlider.oninput = drawPolygons
+
+let kSlider = slider('K', 3, 0.5, 6, 0.5)
+kSlider.oninput = drawPolygons
+
+let aSlider = slider('A', 6, 1, 15)
+aSlider.oninput = drawPolygons
+
+drawPolygons()
